a6dd3c6e02cb4a7c82a12a78b6cffbf313df455d
Enrique1311 <ejspinelli131176@gmail.com>
2023-07-23 20:41:38 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import MaterialUI from "./components/MaterialUI";
+// import Bootstrap from "./components/Bootstrap";
+// import Bulma from "./components/Bulma";
+// import ReactBootstrap from "./components/ReactBootstrap";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+	return (
+		<>
+			<h1>Frameworks de CSS con React</h1>
+			{/* <Bootstrap /> */}
+			{/* <Bulma /> */}
+			{/* <ReactBootstrap /> */}
+			<MaterialUI />
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/MaterialUI.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MaterialCard from "./MaterialCard";
+
+const MaterialUI = () => {
+	return (
+		<>
+			{" "}
+			<h2>MaterialUI</h2>
+			<MaterialCard />
+		</>
+	);
+};
+
+export default MaterialUI;
